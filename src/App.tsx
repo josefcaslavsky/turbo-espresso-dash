22dadd67562cfe1916c0d189a3136279f3aba521
Josef Čáslavský <josef.caslavsky@gmail.com>
2025-07-29 20:52:00 -0600

    gh pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -11,19 +11,19 @@ const queryClient = new QueryClient();
 
 const App = () => {
   const isMobile = useIsMobile();
-  
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         {!isMobile && <Toaster />}
         {!isMobile && <Sonner />}
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
